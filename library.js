47d8a5b63775729d4c90cf880491e03ca17db0fa
floatyears <cl735915647@gmail.com>
2012-03-05 11:59:35 +0800

    增加BOM各个对象的示例，增加浏览器代理检测方法client().

--- library.js
+++ library.js
@@ -1610,3 +1610,134 @@ function ajaxQueue(url,options,queue){
 		//requestQueue[queue][0].request.send(requestQueue[queue][0].send);
 	}
 }
+
+/*
+ * hasPlugin(name)：检查是否有相应的插件
+ * name表示插件的名称
+ */
+function hasPlugin(name){
+	name = name.toLowerCase();
+	if(window.ActiveXObject){
+		try{
+			var a = new ActiveXObject(name);
+			return true;
+		}
+		catch(e){
+			return false;
+		}
+	}else{
+		var plugins = navigator.plugins;
+		var pulgin;
+		for(var i = 0;(plugin = plugins[i]); i++){
+			if(plugin.name.toLowerCase() == name){
+				console.log(plugin.name);
+				return true;
+			}
+			console.log(plugin.name);
+		}
+		return false;
+	}
+}
+
+/*
+ * clien():检测用户代理字符串，包括浏览器，引擎，平台
+ *
+ *
+ */
+function client(){
+	var engine = {
+		ie:0,gecko:0,webkit:0,khtml:0,opera:0,ver:null
+	}
+	var browser = {
+		ie:0,firefox:0,konq:0,opera:0,chrome:0,safari:0,ver:null
+	}
+	var system = {
+		win:false,mac:false,x11:false,iphone:false,ipod:false,nokiaN:false,winMobile:false,macMobile:false,wii:false,ps:false
+	}
+	var ua = navigator.userAgent;
+	if(window.opera){
+		engine.ver = browser.ver = window.opera.version();
+		engine.opera = browser.opera = parseFloat(engin.ver);
+	}else if(/AppleWebkit\/(\S+)/i.test(ua)){
+		engine.ver = RegExp['$1'];
+		engine.webkit = parseFloat(engine.ver);
+		if(/Chrome\/(\S+)/i.test(ua)){
+			browser.ver = RegExp['$1'];
+			browser.chrome = parseFloat(browser.ver);
+		}else if(/Version\/(\S+)/i.test(ua)){
+			browser.ver = RegExp['$1'];
+			browser.safari = parseFloat(browse.ver);
+		}else{
+			var safariVersion = 1;
+			if(engine.webkit < 100){
+				safariVersion = 1;
+			}else if(engine.webkit < 312){
+				safariVersion = 1.2;
+			}else if(engine.webkit < 412){
+				safariVersion < 1.3;
+			}else{
+				safariVersion = 2;
+			}
+			browser.safari = browser.ver = safariVersion;
+		}
+	} else if(/KHTML\/(\S+)/i.test(ua) || /Konqueror\/[^;]+/i.test(ua)){
+		engine.ver = browser.ver = RegExp['$1'];
+		engine.khtml = browser.konq = parseFloat(engine.ver);
+	}else if(/rv:([^\)]+)\) Gecko\/\d{8}/i.test(ua)){
+		engine.ver = RegExp['$1'];
+		engine.gecko = parseFloat(engine.ver);
+		if(/Firefox\/(\S+)/i.test(ua)){
+			browser.ver = RegExp['$1'];
+			browser.firefox = parseFloat(browser.ver);
+		}
+	}else if(/MSIE ([^;]+)/i.test(ua)){
+		engine.ver = browser.ver = RegExp['$1'];
+		engine.ie = browser.ie = parseFloat(browser.ver);
+	}
+	browser.ver = engine.ie;
+	browser.opera = engine.opera;
+	
+	var p = navigator.platform;
+	system.win = p.indexOf('Win') == 0;
+	system.mac = p.indexOf('Mac') == 0;
+	system.x11 = (p == "x11") || (p.indexOf('Linux') == 0);
+	
+	if(system.win){
+		if(/Win(?:dows )?([^do]{2})\s?(\d+\.\d+)?/i.test(ua)){
+			if(RegExp['$1'] == 'NT'){
+				switch(RegExp['$2']){
+					case '5.0':
+						system.win = '2000';
+						break;
+					case '5.1':
+						system.win = 'XP';
+						break;
+					case '6.0':
+						system.win = 'Vista';
+						break;
+					default:
+						system.win = 'NT';
+						break;
+				}
+			}else if(RegExp['$1'] == '9x'){
+				system.win = 'ME';
+			}else{
+				system.win = RegExp['$1'];
+			}
+		}
+	}
+	system.iphone = ua.indexOf('iPhone') > -1;
+	system.ipod = ua.indexOf('iPod') > -1;
+	system.nokiaN = ua.indexOf('NokiaN') > -1;
+	system.winMobile = (system.win == 'CE');
+	system.macMobile = (system.iphone || system.ipod);
+	
+	system.wii = ua.indexOf('Wii') > -1;
+	system.ps = /playstation/i.test(ua);
+	
+	return {
+		engine:engine,
+		browser:browser,
+		system:system
+	}
+}
